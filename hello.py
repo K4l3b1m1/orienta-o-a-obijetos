8a3cd74aa8097dd65b87d2680bc893680e33a588
k4l3b1m1 <kalebimisale2@gmail.com>
2024-05-05 10:40:12 -0300

    Alteração no repositório local

---
+++ hello.py
@@ -0,0 +1,7 @@
+
+name = input('what my name?: ')
+print(f'ok my name is {name} this name is good thank you')
+print('Hello World')
+print('I am alive kkkkkk')
+print('oh This is a creeper ou a enderman?')
+
