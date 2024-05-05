57bcd1e3917bccc73e3197719a34642795436c67
k4l3b1m1 <kalebimisale2@gmail.com>
2024-05-05 10:43:16 -0300

    HHH

--- hello.py
+++ hello.py
@@ -3,5 +3,5 @@ name = input('what my name?: ')
 print(f'ok my name is {name} this name is good thank you')
 print('Hello World')
 print('I am alive kkkkkk')
-print('oh This is a creeper ou a enderman?')
+print('oh MY GOD This is a creeper ou a enderman?')
 
