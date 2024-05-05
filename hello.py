0b6d4a07187684845420b08fff6dbd2091369782
K4l3b1m1 <165394415+K4l3b1m1@users.noreply.github.com>
2024-05-05 10:55:39 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 
 name = input('what my name?: ')
 print(f'ok my name is {name} this name is good thank you')
-print('Hello Worl')
+print(f'Hello WorlD I AM A {name}')
 print('I am alive kkkkkk')
 print('oh MY GOD This is a creeper ou a enderman?')
 print('I AM ')
